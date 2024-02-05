21b3fa6ee6da4c6f42ebc795bd14c7c3699be8db
harshisjelly2010 <142248356+harshisjelly2010@users.noreply.github.com>
2024-02-05 19:10:12 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
-# import the necessary modules
 from flask import Flask , render_template , request , jsonify
-
-# importing sentiment_analysis file as sa
 import sentiment_analysis as sa
 
 app = Flask(__name__)
 
-# app route for index page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# write a route for post request
-@app.route('' , methods = [''])
-def review():
 
-    # extract the customer_review by writing the appropriate 'key' from the JSON data
-    review = request.json.get('')
+@app.route('/predict' , methods = ['POST'])
+def predict():
 
-    # check if the customer_review is empty, return error
+    response = ""
+    review = request.json.get('customer_review')
+
     if not review:
 
-        return jsonify({'status' : 'error' , 
-                        'message' : 'Empty response'})
-
-    # if review is not empty, pass it through the 'predict' function.
-    # predict function returns 2 things : sentiment and path of image in static folder
-    # example : Positive , ./static/assets/emoticons/positive.png
+        response = jsonify({'status' : 'Error' , 
+                            'message' : 'Empty Review'})
 
     else:
 
-        _ , _ = sa.predict(review)
+        sentiment , emoji_url = sa.predict(review)
+        response = jsonify({'status' : 'Success',
+                            'prediction' : sentiment,
+                            'url' : emoji_url})
 
-        return jsonify({'':'' , '':''})
-
+    return response
 
 if __name__  ==  "__main__":
     app.run(debug = True)
